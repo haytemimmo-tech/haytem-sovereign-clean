b005730eeaaf42f0796c0bc893ddb23f2de0bde0
haytemimmo-tech <haytem.immo@gmail.com>
2026-05-11 07:55:05 +0100

    Add sitemap generation for website URLs

---
+++ app/sitemap.ts
@@ -0,0 +1,126 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.haytemsovereign.com',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/#about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/#advisory',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/intelligence',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/#eldorado',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/journal',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/albania-riviera-2026-investment-thesis',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/marrakech',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/tangier',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/casablanca-marina',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/batumi',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/porto-montenegro',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/lustica-bay',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/vlora',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/durr-s',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/ksamil',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/athens-riviera',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/bali',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/mauritius',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/cape-verde',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/tulum',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/buenos-aires',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/singapore',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/abu-dhabi',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/ras-al-khaimah',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/maldives',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/cyprus',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/estepona',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/eldorado/marbella',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://www.haytemsovereign.com/#mandate',
+      lastModified: new Date(),
+    },
+  ]
+}
